6c85e53ee76085ff882404e6823663e01faa7c89
zhisz <152690516+zhisz@users.noreply.github.com>
2025-08-17 20:20:04 +0800

    111

--- justfw/app/EnginX/chassis/chassis.c
+++ justfw/app/EnginX/chassis/chassis.c
@@ -45,9 +45,9 @@ void chassis_MainLoop()
         while (chassis_rc_ctrl[0].rc.switch_left==1)
         {
             // printf("USB_OK");
-            speed_x = chassis_rc_ctrl[0].rc.rocker_l_ / 660.0f * 10;
-            speed_y = chassis_rc_ctrl[0].rc.rocker_l1 / 660.0f * 10;
-            speed_w = -chassis_rc_ctrl[0].rc.dial / 660.0f * 3 + (chassis_rc_ctrl[0].rc.rocker_r_ / 660.0f * 10);
+            speed_x = chassis_rc_ctrl[0].rc.rocker_l_ / 660.0f ;
+            speed_y = chassis_rc_ctrl[0].rc.rocker_l1 / 660.0f ;
+            speed_w = -chassis_rc_ctrl[0].rc.dial / 660.0f * 3 + (chassis_rc_ctrl[0].rc.rocker_r_ / 660.0f );
 
             Set_Speed(speed_x, speed_y, speed_w);
 
